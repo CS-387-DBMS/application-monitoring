26034f4a993b5cb86239fe4bfa440c2214801dfe
Kowndinya Renduchintala <64963391+kowndinya-renduchintala@users.noreply.github.com>
2022-04-28 19:51:46 +0530

    request_log.py
    request logs from machines to be monitored

---
+++ request_log.sh
@@ -0,0 +1,6 @@
+python3 request_log.py \
+    --ip_addresses 10.42.0.151 127.0.0.1 \
+    --tokens FgWMg-pwxFwwg17d3XGYAvUg_nX7jI1uh9k0EFqeEekndEz5PZArM6-WLgmJZ2V3jTq4Fd_eFVvrcw0teSOUOA== S0Eo9ZlbLds4RyfUTsWwS1amGVXjPzdj_UZstGRePFGBTLFq_XCVvmkkvYG_Fvo62mfG2mHluqYJCmSoJnwI-w== \
+    --orgs dbproject CS387-Project \
+    --buckets apm APM \
+    --ports 8086 8086 
