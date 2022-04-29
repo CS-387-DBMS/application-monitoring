d6a02a3133aca7b894338eab7eda4587937a0a37
kowndinya-renduchintala <rharisrikowndinya333@gmail.com>
2022-04-30 02:38:44 +0530

    upload log files to influxdb

--- backend/my_backend/basic_input/views.py
+++ backend/my_backend/basic_input/views.py
@@ -1,5 +1,5 @@
 from itertools import count
-from os import stat
+import os
 from django.shortcuts import render
 from django.http import HttpResponse, JsonResponse
 from django.views.decorators.csrf import csrf_exempt
@@ -11,6 +11,10 @@ from basic_input.my_thread import GetLogsThread
 from threading import Event
 from subprocess import run
 from json import loads
+import influxdb_client
+from influxdb_client.client.write_api import SYNCHRONOUS
+import datetime
+import pandas as pd
 
 # Create your views here.
 
@@ -47,7 +51,20 @@ def getLogs(machine_obj):
     # pray()
 
     # read csv log files, insert into infux
-    
+    client=influxdb_client.InfluxDBClient(
+        url="http://localhost:8086"
+        token=os.environ["influxdb_token"],
+        org=os.environ["influxdb_org"]
+    )
+    write_api=client.write_api(write_options=SYNCHRONOUS)
+    columns=['timestamp', 'type', 'cpu_used', 'total_cpu_used', 'mem_used', 'total_mem_used', 'total_swap_used', 'disk_read', 'disk_write', 'total_disk_read', 'total_disk_write', 'bytes_sent', 'bytes_recv', 'total_bytes_sent', 'total_bytes_recv', 'total_packets_sent', 'total_packets_recv', 'total_dropin', 'total_dropout']
+    data=pd.read_csv("/tmp/host.log", names=columns)
+    data.rename(columns={"timestamp":"_time"}, inplace=True)
+    data["_time"]=data["_time"].apply(lambda x: datetime.datetime.fromtimestamp(int(x)).astimezone())
+    data=data.set_index("_time")
+    data.drop(columns=['type'], inplace=True, axis=1)
+    # print(data)
+    write_api.write(bucket=os.environ["influxdb_bucket"], record=data, data_frame_measurement_name=ip)
     return True
 
 
